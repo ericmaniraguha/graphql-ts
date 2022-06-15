b083af7cf56312d9445723c7d9492940c8b62ef3
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-06-15 20:12:10 +0200

    done by config. apolloServer

--- readme.md
+++ readme.md
@@ -14,4 +14,13 @@ start:
 
 6. setting up express server
 
-npm i express
+npm i express 7. run the app "run start dev"
+
+# =====================Configure GraphQL Apollo=============================================
+
+1. npm install apollo-server-express graphql type-graphql
+
+2. npm i reflect-metadata
+
+3. after running the app, use check on the url our apollo server using :
+   "http://localhost:8000/graphql"
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
+import 'reflect-metadata';
 import express, { Express } from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { TaskResolver } from './resolvers/task';
 
 const main = async () => {
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [TaskResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
   const app: Express = express();
+
+  apolloServer.applyMiddleware({ app });
 
   app.get('/', (_req, res) => res.send('Hello World'));
 
